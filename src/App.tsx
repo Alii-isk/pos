32fe87d2ad3920dc63d6e0470796d9f8f17c8554
Alii isk <alii.isk800@gmail.com>
2023-09-15 18:40:24 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'antd';
+
+const App: React.FC = () => (
+  <div className="flex flex-col bg-gray-500 p-12">
+    <Button type="dashed" >ssdn</Button>
+    <Button type="dashed" >Button</Button>
+
+  </div>
+);
+
+export default App;
